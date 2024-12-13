ce2da785f850874f1df8169898e1894846fa39b2
AndrewDRichter <andrewrichter1@hotmail.com>
2024-12-13 17:27:01 -0300

    everything setted up for authentication requests, now let's proceed to develop the login page and apply the authentication to it

--- api/service.py
+++ api/service.py
@@ -1,4 +1,22 @@
-import requests as re
+import requests
 
-response = re.get('https://andrewrichter.pythonanywhere.com/api/v1/movies')
-print(response.content)
+
+class Auth:
+
+    def __init__(self):
+        self.__base_url = 'http://andrewrichter.pythonanywhere.com/api/v1/'
+        self.__auth_url = f'{self.__baseurl}authentication/token/'
+
+    def get_token(self, username, password):
+        auth_payload = {
+            'username': username,
+            'password': password
+        }
+        auth_response = requests.post(
+            self.__auth_url,
+            data=auth_payload,
+        )
+
+        if auth_response.status_code == 200:
+            return auth_response.json()
+        return {'error': f'Erro de autenticação. Status code: {auth_response.status_code}'}
