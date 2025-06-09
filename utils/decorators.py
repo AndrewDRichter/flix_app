e1904803a1e45310e36419eda9e9f461c6d8db40
AndrewDRichter <andrewrichter1@hotmail.com>
2025-06-09 17:15:16 -0300

    ok

--- utils/decorators.py
+++ utils/decorators.py
@@ -19,8 +19,8 @@ def login_decorator(func):
         '''
         Your api should return a token string containing your authentication token.
 
-        If you are already logged in the token contained in st.session_state
-        will bypass the if statement and call the function decorated in the else statement.
+        If you are already logged in, the token contained in st.session_state
+        will bypass the if statement and call the function wrapped by this decorator.
         '''
         if 'token' not in st.session_state:
             show_login()
